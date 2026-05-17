1626cbb59395513ecce2fa3af3b315b75b630282
Joe Imhoff <joe.imhoff@outlook.com>
2026-05-16 20:06:39 -0500

    Adding more basics
    - Drizzle installed
    - Bare database file installed
    - Single test and npm check pass

---
+++ drizzle/0000_striped_dragon_man.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`email` text NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [sveltekit()],
@@ -11,5 +11,8 @@ export default defineConfig({
   preview: {
     port: 6300,
     strictPort: true
+  },
+  test: {
+    include: ['tests/**/*.{test,spec}.{js,ts}']
   }
 });
